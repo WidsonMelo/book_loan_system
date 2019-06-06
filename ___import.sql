367e2aa47eb0caea0b911e8935e886e302d7e755
Widson Melo <widsonmelo@gmail.com>
2019-06-05 23:26:00 -0300

    Correcao no MySQL

--- ___import.sql
+++ ___import.sql
@@ -5,14 +5,11 @@ CREATE TABLE `amigo` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-
-
 CREATE TABLE `role` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `role` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
 
 CREATE TABLE `task` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -45,51 +42,5 @@ CREATE TABLE `user_task` (
   CONSTRAINT `id_usertask_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `id_usertask_task_id` FOREIGN KEY (`task_id`) REFERENCES `task` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
-CREATE TABLE amigo (id int(11) NOT NULL AUTO_INCREMENT, email varchar(255) NOT NULL, nome varchar(255) NOT NULL, PRIMARY KEY (id)) DEFAULT CHARSET=utf8;
-
-CREATE TABLE role (id int(11) NOT NULL AUTO_INCREMENT, role varchar(255) DEFAULT NULL, PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-CREATE TABLE task (id int(11) NOT NULL AUTO_INCREMENT, name varchar(45) DEFAULT NULL, description varchar(45) DEFAULT NULL, date_created datetime DEFAULT NULL, finished bit(1) DEFAULT b'0', PRIMARY KEY (id)) ENGINE=INNODB AUTO_INCREMENT DEFAULT CHARSET=utf8;
-CREATE TABLE user (id int(11) NOT NULL AUTO_INCREMENT, active int(11) DEFAULT 1, email varchar(255) NOT NULL, name varchar(255) NOT NULL, password varchar(255) NOT NULL, role_id int(11) DEFAULT NULL, PRIMARY KEY (id), KEY id_role_id (role_id), CONSTRAINT id_role_id FOREIGN KEY (role_id) REFERENCES role (id)) ENGINE=InnoDB AUTO_INCREMENT DEFAULT CHARSET=utf8;
-CREATE TABLE user_task (id int(11) not NULL AUTO_INCREMENT, user_id int(11)  NOT NULL, task_id int(11)  NOT NULL, PRIMARY KEY (id), KEY id_usertask_user_id (user_id), KEY id_usertask_task_id (task_id), CONSTRAINT id_usertask_user_id FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE ON UPDATE CASCADE, CONSTRAINT id_usertask_task_id FOREIGN KEY (task_id) REFERENCES task (id) ON DELETE CASCADE ON UPDATE CASCADE) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 INSERT INTO role VALUES (1,'ADMIN');
-
 INSERT INTO `role` VALUES (2,'USER');
-
-INSERT INTO amigo VALUES (1,'amigo@amigo', 'nome');
-
-INSERT INTO amigo (nome, email) VALUES ('xoxo', 'xoxo@xoxo');
-DELETE FROM role WHERE id = 2;
-
-
-
-
-CREATE TABLE `task` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
-  `description` varchar(45) DEFAULT NULL,
-  `date_created` datetime DEFAULT NULL,
-  `finished` bit(1) DEFAULT b'0',
-  PRIMARY KEY (`id`)
-) ENGINE=INNODB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `active` int(11) DEFAULT NULL,
-  `email` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `role_id` int(11) DEFAULT 1,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
-
-CREATE TABLE `user_task` (
-	`id` int(11) not NULL AUTO_INCREMENT,
-  `user_id` int(11)  NOT NULL,
-  `task_id` int(11)  NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_usertask_user_id` (`user_id`),
-  KEY `id_usertask_task_id` (`task_id`),
-  CONSTRAINT `id_usertask_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `id_usertask_task_id` FOREIGN KEY (`task_id`) REFERENCES `task` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
